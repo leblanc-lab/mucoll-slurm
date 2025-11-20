4f4499864601a51149bcbb145dd2717479db0612
Matt LeBlanc <matt_leblanc@brown.edu>
2025-11-20 15:33:32 -0500

    Adding setup scripts for OSCAR

---
+++ scripts/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source /opt/spack/opt/spack/__spack_path_placeholder__/__spack_path_placeholder__/__spack_path_placeholder__/__spack_path_placeholder__/linux-x86_64/mucoll-stack-2025-11-11-qnljda2jpzad6mfv26pge4ux75mnbl3v/setup.sh
+
+export PS1="[\u@\h \w]\$ "
+alias ls='ls --color=auto'
---
+++ scripts/shell.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+apptainer shell --cleanenv docker://ghcr.io/muoncollidersoft/mucoll-sim-alma9:full_gaudi_test 
